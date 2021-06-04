a8101b83f493597abea301951e81fe729d5068d7
HalehGhavami <79171561+HalehGhavami@users.noreply.github.com>
2021-06-04 18:25:05 +0200

    commented some MovieCard PropTypes

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -20,19 +20,11 @@ export class MovieCard extends React.Component {
 MovieCard.propTypes = {
   movie: PropTypes.shape({
     Title: PropTypes.string.isRequired,
-    Description: PropTypes.string.isRequired,
-    Genre: PropTypes.shape({
-      Name: PropTypes.string.isRequired,
-      Description: PropTypes.string.isRequired,
-    }),
-    Director: PropTypes.shape({
-      Name: PropTypes.string.isRequired,
-      Bio: PropTypes.string.isRequired,
-      Birth: PropTypes.string.isRequired,
-      Death: PropTypes.string.isRequired,
-    }),
-    ImagePath: PropTypes.string.isRequired,
-    Featured: PropTypes.bool.isRequired,
+    // Description: PropTypes.string.isRequired,
+    // ImagePath: PropTypes.string.isRequired,
+    // Genre: PropTypes.shape({
+    //   Name: PropTypes.string,
+    // }),
   }).isRequired,
   onMovieClick: PropTypes.func.isRequired,
 };
