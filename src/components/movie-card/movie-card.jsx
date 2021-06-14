af86db013f72252f78044b26e541e2a5d007a93d
HalehGhavami <79171561+HalehGhavami@users.noreply.github.com>
2021-06-14 15:49:13 +0200

    added Link and undated PropTypes

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-//Components & Styling
 import Button from 'react-bootstrap/Button';
+
 import './movie-card.scss';
+import { Link } from 'react-router-dom';
+
 export class MovieCard extends React.Component {
   render() {
-    const { movie, onMovieClick } = this.props;
+    const { movie } = this.props;
+
     return (
       <div className="movie-card bg-white m-2 p-2 rounded d-flex flex-column justify-content-between align-items-center">
         <div className="d-flex flex-column align-items-center">
@@ -20,23 +23,22 @@ export class MovieCard extends React.Component {
         <p className="movie-card-description text-muted">
           {movie.Description.slice(0, 90)}...
         </p>
-        <Button
-          className="m-1"
-          variant="info"
-          onClick={() => onMovieClick(movie)}
-        >
-          View More
-        </Button>
+
+        <Link to={`/movies/${movie._id}`}>
+          <Button className="m-1" variant="info">
+            Open
+          </Button>
+        </Link>
       </div>
     );
   }
 }
 
-// PropTypes
 MovieCard.propTypes = {
   movie: PropTypes.shape({
     Title: PropTypes.string.isRequired,
     Description: PropTypes.string.isRequired,
+    ImagePath: PropTypes.string.isRequired,
     Genre: PropTypes.shape({
       Name: PropTypes.string.isRequired,
       Description: PropTypes.string.isRequired,
@@ -44,11 +46,8 @@ MovieCard.propTypes = {
     Director: PropTypes.shape({
       Name: PropTypes.string.isRequired,
       Bio: PropTypes.string.isRequired,
-      Birth: PropTypes.string,
+      Birth: PropTypes.string.isRequired,
+      Death: PropTypes.string,
     }),
-    ImagePath: PropTypes.string.isRequired,
-    Featured: PropTypes.bool.isRequired,
   }).isRequired,
 };
-
-export default MovieCard;
