3c34be99b183dc8de7de5d0a4d57512760e8b5ba
HalehGhavami <79171561+HalehGhavami@users.noreply.github.com>
2021-06-14 15:36:18 +0200

    added POST request to the login endpoint using Axios

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,40 +1,56 @@
 import React, { useState } from 'react';
-import PropTypes from 'prop-types';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-
+import Container from 'react-bootstrap/Container';
 import './login-view.scss';
+import { Form, Button } from 'react-bootstrap';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
 export function LoginView(props) {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
+  //The extra (e) in the code, as well as the e.preventDefault(); method, prevents the default refresh/change of the page from the handleSubmit() method.
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(username, password);
-    /* Send a request to the server for authentication */
-    /* then call props.onLoggedIn(username) */
-    props.onLoggedIn(username);
+    // Send a request to the server for authentication
+    axios
+      .post('https://api-myflix.herokuapp.com/login', {
+        Username: username,
+        Password: password,
+      })
+      .then((response) => {
+        const data = response.data;
+        props.onLoggedIn(data);
+      })
+      .catch((e) => {
+        console.log('no such user');
+      });
   };
 
   return (
-    <div className="center">
+    <Container className="login-view center" fluid="true">
       <h1 className="title">myFlix</h1>
       <Form>
         <Form.Group controlId="formUsername">
-          <Form.Label>Username:</Form.Label>
+          <Form.Label></Form.Label>
           <Form.Control
             type="text"
+            value={username}
+            placeholder="Enter Username"
             onChange={(e) => setUsername(e.target.value)}
           />
         </Form.Group>
 
         <Form.Group controlId="formPassword">
-          <Form.Label>Password:</Form.Label>
+          <Form.Label></Form.Label>
           <Form.Control
             type="password"
+            placeholder="Enter Password"
+            value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
         </Form.Group>
+
         <div className="middle">
           <Button
             className="m-3"
@@ -44,16 +60,19 @@ export function LoginView(props) {
           >
             Login
           </Button>
+          <Form.Group
+            className="registration-group"
+            controlId="formRegistration"
+          >
+            <Form.Text className="text-light">Don't have an account?</Form.Text>
+            <Link to={`/register`}>
+              <Button className="register-link" variant="dark">
+                Register here
+              </Button>
+            </Link>
+          </Form.Group>
         </div>
       </Form>
-    </div>
+    </Container>
   );
 }
-
-LoginView.propTypes = {
-  user: PropTypes.shape({
-    Username: PropTypes.string.isRequired,
-    Password: PropTypes.string.isRequired,
-  }),
-  onLoggedIn: PropTypes.func.isRequired,
-};
