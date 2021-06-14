5b7fc522219024a85366b7c07b517e531986c7ca
HalehGhavami <79171561+HalehGhavami@users.noreply.github.com>
2021-06-14 16:01:09 +0200

    added director-view to get director information straight from a movie

---
+++ src/components/director-view/director-view.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Button, Container } from 'react-bootstrap';
+import PropTypes from 'prop-types';
+import './director-view.scss';
+
+export class DirectorView extends React.Component {
+  render() {
+    const { director, onBackClick } = this.props;
+    return (
+      <Container fluid className="DirectorView">
+        <div className="director-view">
+          <div className="director-name">
+            <span className="label font-weight-bold">Director: </span>
+            <span className="value">{director.Name}</span>
+          </div>
+          <div className="director-bio">
+            <span className="label font-weight-bold">Bio: </span>
+            <span className="value">{director.Bio}</span>
+          </div>
+          <div className="director-birth">
+            <span className="label font-weight-bold">Born: </span>
+            <span className="value">{director.Birth}</span>
+          </div>
+          <Button
+            variant="info"
+            size="sm"
+            onClick={() => {
+              onBackClick(null);
+            }}
+          >
+            Back
+          </Button>
+        </div>
+      </Container>
+    );
+  }
+}
+
+DirectorView.propTypes = {
+  director: PropTypes.shape({
+    Name: PropTypes.string.isRequired,
+    Bio: PropTypes.string.isRequired,
+    Birth: PropTypes.string.isRequired,
+  }),
+  onBackClick: PropTypes.func.isRequired,
+};
