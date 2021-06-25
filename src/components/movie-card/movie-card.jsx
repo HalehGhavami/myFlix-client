b66e4b06788ea4620f316cddeae82e0f56e9ba60
HalehGhavami <79171561+HalehGhavami@users.noreply.github.com>
2021-06-25 23:18:51 +0200

    deleted unused codes

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
 
 import './movie-card.scss';
 import { Link } from 'react-router-dom';
