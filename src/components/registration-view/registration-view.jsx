ac5dbb1d2c1f127f06e508a4b4c729b30a75336e
HalehGhavami <79171561+HalehGhavami@users.noreply.github.com>
2021-06-25 21:12:33 +0200

    called the useState() method+ refactor the code +added propTypes

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -4,85 +4,95 @@ import './registration-view.scss';
 import Container from 'react-bootstrap/Container';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import PropTypes from 'prop-types';
 
 import './registration-view.scss';
 
-export function RegistrationView() {
-  //call the useState() method (imported from React) with an empty string This method returns an array that you destructure (break down into variables)
+export function RegistrationView(props) {
+  // hooks update function state
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [email, setEmail] = useState('');
   const [birthday, setBirthday] = useState('');
 
-  const onRegister = (e) => {
+  const handleRegister = (e) => {
     e.preventDefault();
-    axios
-      .post('https://api-myflix.herokuapp.com/users', {
-        Username: username,
-        Password: password,
-        Email: email,
-        Birthday: birthday,
-      })
-      .then((response) => {
-        const data = response.data;
-        console.log(data);
-        window.open('/', '_self'); // the second argument '_self' is necessary so that the page will open in the current tab
-      })
-      .catch((e) => {
-        console.log('error registering the user');
-      });
-
-    // console.log(username, password, email, birthday);
-    // props.onRegister();
+    if (username.length < 5) {
+      alert('Username must be longer than 5 characters.');
+    } else if (!email.includes('@') || !email.includes('.')) {
+      alert('Please use a valid email address.');
+    } else if (password.length < 5) {
+      alert('Please enter password with at least 5 characters.');
+    } else {
+      axios
+        .post('https://api-myflix.herokuapp.com/users', {
+          Username: username,
+          Password: password,
+          Email: email,
+          Birthday: birthday,
+        })
+        .then((response) => {
+          const data = response.data;
+          console.log(data);
+          window.open('/', '_self'); // the second argument '_self' is necessary so that the page will open in the current tab
+        })
+        .catch((e) => {
+          console.log('error registering the user: ' + e);
+          alert('Something went wrong.');
+        });
+    }
   };
 
   return (
     <Container className="registration-view center mt-5" fluid="true">
-      <h1 className="myflix-title">myFlix Registration</h1>
+      <h1 className="myflix-title">Welcome to myFlix Registration</h1>
       <Form>
-        <Form.Group controlId="formText">
+        <Form.Group controlId="formBasicText">
           <Form.Label>username</Form.Label>
           <Form.Control
             type="text"
-            placeholder="Enter username"
+            placeholder="Enter Username"
+            required
             value={username}
             onChange={(e) => setUsername(e.target.value)}
           />
         </Form.Group>
-        <Form.Group controlId="formPassword">
+        <Form.Group controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
           <Form.Control
             type="password"
             placeholder="Password"
+            required
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
         </Form.Group>
-        <Form.Group controlId="formEmail">
+        <Form.Group controlId="formBasicEmail">
           <Form.Label>Email</Form.Label>
           <Form.Control
             type="email"
+            placeholder="name@example.com"
+            required
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            placeholder="Enter Email"
           />
         </Form.Group>
-        <Form.Group controlId="formBirthday">
+        <Form.Group controlId="formBasicBirthday">
           <Form.Label>Birthday</Form.Label>
           <Form.Control
             type="date"
+            required
             value={birthday}
             onChange={(e) => setBirthday(e.target.value)}
-            placeholder="Enter Birthday"
           />
         </Form.Group>
         <Button
           className="submit-button"
           variant="info"
           type="submit"
-          onClick={onRegister}
+          onClick={handleRegister}
         >
-          Submit
+          Register
         </Button>
 
         <Form.Group className="login-group" controlId="formLogin">
@@ -97,3 +107,12 @@ export function RegistrationView() {
     </Container>
   );
 }
+
+RegistrationView.propTypes = {
+  user: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+    password: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+    birthdate: PropTypes.string,
+  }),
+};
